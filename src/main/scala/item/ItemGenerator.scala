5239fc3e047888f1e326b3e0223e460ecf164bb6
bbonnet <none>
2015-05-12 14:01:21 +0200

    Refactor: extract function for character mapping

--- src/main/scala/item/ItemGenerator.scala
+++ src/main/scala/item/ItemGenerator.scala
@@ -2,36 +2,37 @@ package item
 
 object ItemGenerator {
 
-  val items = Array(Medal, BootsOfSpeed, HeadGearArmorItem, RingOfProtection, RingOfFireResistance, RingOfSpellTurning, GauntletsOfOgrePower, Anklet, Brooch, Orb)
+	val items = Array(Medal, BootsOfSpeed, HeadGearArmorItem, RingOfProtection, RingOfFireResistance, RingOfSpellTurning, GauntletsOfOgrePower, Anklet, Brooch, Orb)
 
-  val charCategories = Map(
-    0 -> List('A', 'F', 'K', 'P', 'U', 'Z'),
-    1 -> List('B', 'G', 'L', 'Q', 'V'),
-    2 -> List('C', 'H', 'M', 'R', 'W'),
-    3 -> List('D', 'I', 'N', 'S', 'X'),
-    4 -> List('E', 'J', 'O', 'T', 'Y'),
-    5 -> List('0', '5'),
-    6 -> List('1', '6'),
-    7 -> List('2', '7', '!', '?', '-', '&'),
-    8 -> List('3', '8'),
-    9 -> List('4', '9'))
+	val charCategories = Map(
+		0 -> List('A', 'F', 'K', 'P', 'U', 'Z'),
+		1 -> List('B', 'G', 'L', 'Q', 'V'),
+		2 -> List('C', 'H', 'M', 'R', 'W'),
+		3 -> List('D', 'I', 'N', 'S', 'X'),
+		4 -> List('E', 'J', 'O', 'T', 'Y'),
+		5 -> List('0', '5'),
+		6 -> List('1', '6'),
+		7 -> List('2', '7', '!', '?', '-', '&'),
+		8 -> List('3', '8'),
+		9 -> List('4', '9'))
 
-  def generateFirstItem(name: String): Item = {
-    val digits =
-      for {
-        inputChar <- name.toUpperCase()
-        charCategoryOption = charCategories.find { _._2 contains inputChar }
-        charCategory = if (charCategoryOption.isDefined) charCategoryOption.get._1 else 0
-      } yield charCategory
-    items(reduceToOneDigit(digits.sum))
-  }
+	def generateFirstItem(name: String): Item = {
+		val digits = name.toUpperCase().map { charCategory(_).getOrElse(0) }
+		items(reduceToOneDigit(digits.sum))
+	}
 
-  private def reduceToOneDigit(number: Int): Int = {
-    val numberString = number.toString()
-    if (numberString.length < 2) number
-    else {
-      val digitsInNumber = numberString.map { _.toString.toInt }
-      reduceToOneDigit(digitsInNumber.sum)
-    }
-  }
+	private def charCategory(char: Char): Option[Int] = {
+		val charCategoryOption = charCategories.find { _._2 contains char }
+		if (charCategoryOption.isDefined) Some(charCategoryOption.get._1)
+		else None
+	}
+
+	private def reduceToOneDigit(number: Int): Int = {
+		val numberString = number.toString()
+		if (numberString.length < 2) number
+		else {
+			val digitsInNumber = numberString.map { _.toString.toInt }
+			reduceToOneDigit(digitsInNumber.sum)
+		}
+	}
 }
--- src/test/scala/item/ItemGeneratorTest.scala
+++ src/test/scala/item/ItemGeneratorTest.scala
@@ -4,23 +4,23 @@ import org.scalatest.FunSuite
 import org.scalatest.Matchers
 
 class ItemGeneratorTest extends FunSuite with Matchers {
-  test("Should return Medal When generating first item Given a") {
-    ItemGenerator.generateFirstItem("a") shouldBe Medal
-  }
+	test("Should return Medal When generating first item Given a") {
+		ItemGenerator.generateFirstItem("a") shouldBe Medal
+	}
 
-  test("Should return Boots of speed When generating first item Given b") {
-    ItemGenerator.generateFirstItem("b") shouldBe BootsOfSpeed
-  }
+	test("Should return Boots of speed When generating first item Given b") {
+		ItemGenerator.generateFirstItem("b") shouldBe BootsOfSpeed
+	}
 
-  test("Should return Medal When generationg first item Given Zapuk") {
-    ItemGenerator.generateFirstItem("Zapuk") shouldBe Medal
-  }
+	test("Should return Medal When generationg first item Given Zapuk") {
+		ItemGenerator.generateFirstItem("Zapuk") shouldBe Medal
+	}
 
-  test("Should return BootsOfSpeed When generating first item Given Sofia") {
-	  ItemGenerator.generateFirstItem("Sofia") shouldBe BootsOfSpeed
-  }
-  
-  test("Should return BootsOfSpeed When generating first item Given !?-&") {
-    ItemGenerator.generateFirstItem("!?-&") shouldBe BootsOfSpeed
-  }
+	test("Should return BootsOfSpeed When generating first item Given Sofia") {
+		ItemGenerator.generateFirstItem("Sofia") shouldBe BootsOfSpeed
+	}
+
+	test("Should return BootsOfSpeed When generating first item Given !?-&") {
+		ItemGenerator.generateFirstItem("!?-&") shouldBe BootsOfSpeed
+	}
 }

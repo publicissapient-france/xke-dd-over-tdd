3258c0331524183b7ef366db24d05015c2cf5dc7
Peter Onneby <ponneby@xebia.fr>
2015-05-31 19:09:00 +0200

    Numbers to numbers

--- spec/dungeon_spec.rb
+++ spec/dungeon_spec.rb
@@ -1,12 +1,17 @@
 def convert(letter_in)
   letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  number = letters.index(letter_in)
-  return number - 25 if number > 24
-  return number - 20 if number > 19
-  return number - 15 if number > 14
-  return number - 10 if number > 9
-  return number - 5 if number > 4
-  number
+  numberFromLetter = letters.index(letter_in)
+  if numberFromLetter == nil
+    numberOfLetter = letter_in.to_i
+    return numberOfLetter + 5 if numberOfLetter < 4
+    return numberOfLetter
+  end
+  return numberFromLetter - 25 if numberFromLetter > 24
+  return numberFromLetter - 20 if numberFromLetter > 19
+  return numberFromLetter - 15 if numberFromLetter > 14
+  return numberFromLetter - 10 if numberFromLetter > 9
+  return numberFromLetter - 5 if numberFromLetter > 4
+  numberFromLetter
 end
 
 describe "The Dungeon" do
@@ -31,6 +36,18 @@ describe "The Dungeon" do
         expect(convert(letter)).to be(number)
       end
     end
+  end
 
+  context "numbers to numbers" do
+    {
+      "0" => 5,
+      "1" => 6,
+      "5" => 5,
+      "6" => 6
+    }.each_pair do |number, toNumber|
+      it "converts #{number} to #{toNumber}" do
+        expect(convert(number)).to be(toNumber)
+      end
+    end
   end
 end
